7e6ee9e372a7a9ee8c0b1dc238ac9c3d451ff0c9
☀️ <sumeya4hmed@gmail.com>
2025-01-30 15:50:34 +0000

    works on build

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,40 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: '\u0015Squats Song Form',
-      short_name: 'Squat Form',
-      description: 'We love shaking things up with a good song during our morning ritual squats! Have a tune that gets you moving? Share it with us!',
-      theme_color: '#1f1f1f',
-    },
+      manifest: {
+        name: "\u0015Squats Song Form",
+        short_name: "Squat Form",
+        description:
+          "We love shaking things up with a good song during our morning ritual squats! Have a tune that gets you moving? Share it with us!",
+        theme_color: "#1f1f1f",
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
