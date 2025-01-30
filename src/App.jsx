746304da5abd57282c97fcc6ec801b6a980e85d0
chargrace <lottie808@hotmail.co.uk>
2025-01-30 22:54:58 +0000

    app provider moved to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import Form from "./components/Form/Form.jsx";
 import Playlist from "./components/Playlist/Playlist.jsx";
 import "./App.css";
 import Randomiser from "./components/Randomiser/Randomiser.jsx";
+import { AppProvider } from "./context/AppProvider.jsx";
 
 function App() {
   return (
     <main className="card">
+      <AppProvider>
       <Header />
       <Randomiser />
       <Form />
       <Playlist />
       <PWABadge />
+      </AppProvider>
     </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { AppProvider } from "./context/AppProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppProvider>
       <App />
-    </AppProvider>
   </StrictMode>
 );
