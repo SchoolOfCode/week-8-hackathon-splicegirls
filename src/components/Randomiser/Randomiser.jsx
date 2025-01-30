cadee9caa8ae04eaeeb53836593c3a47521753a6
☀️ <sumeya4hmed@gmail.com>
2025-01-30 21:22:56 +0000

    get random song component added

--- src/components/Randomiser/Randomiser.jsx
+++ src/components/Randomiser/Randomiser.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import { useAppContext } from "../../context/AppProvider";
+
 export default function Randomiser() {
+  const { songs } = useAppContext();
+  const [randomSong, setRandomSong] = useState({});
+
+  function getRandomSong() {
+    const index = Math.floor(Math.random() * songs.length);
+    setRandomSong(songs[index]);
+  }
+
   return (
-    <>
-      <p>Song info goes here</p>
-      <button type="submit" role="button" aria-label="Squat Song Choice">
-        Squat Song Choice
-      </button>
-    </>
+    <section>
+      {/* display random song */}
+      <div>
+        <p>Name: {randomSong.name}</p>
+        <p>Song Title: {randomSong.songTitle}</p>
+        <p>Link: {randomSong.link}</p>
+        <p>Comment: {randomSong.comment}</p>
+      </div>
+      {/* click button to get random song */}
+      <button onClick={() => getRandomSong()}>Randomise!</button>
+    </section>
   );
 }
