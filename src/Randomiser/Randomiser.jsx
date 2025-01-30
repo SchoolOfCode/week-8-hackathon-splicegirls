650aaed829f44bceaf2f5fd70452fbdc4189c7ee
Holly <holly.cello@gmail.com>
2025-01-30 16:37:01 +0000

    Add Initial Files
    Playlist and Randomiser

---
+++ src/Randomiser/Randomiser.jsx
@@ -0,0 +1,8 @@
+export default function Randomiser() {
+    return (
+        <>
+            <p>Song info goes here</p>
+            <button type="submit" role="button" aria-label="Squat Song Choice">Squat Song Choice</button>
+        </>
+    )
+}
