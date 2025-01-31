5e0a7faa844f283de59d6669f7effba2e53612ce
chargrace <lottie808@hotmail.co.uk>
2025-01-31 10:03:45 +0000

    carousel added to playlist

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,19 +1,29 @@
 import { useAppContext } from "../../context/AppProvider";
-import classes from './Playlist.module.css'
+import classes from './Playlist.module.css';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
 
 export default function Playlist() {
   const { songs } = useAppContext();
 
   return (
-    <section className={classes.Container}>
-      {songs.map((song, index) => (
-        <ul key={index}>
-          <p>Name: {song.name}</p>
-          <p>Song Title: {song.songTitle}</p>
-          <p>Link: {song.link}</p>
-          <p>Comment: {song.comment}</p>
-        </ul>
-      ))}
-    </section>
+    // <section className={classes.Container}>
+    <>
+      <Carousel>
+        {songs.map((song, index) => (
+          <Paper 
+            key={index} 
+            className={classes.Cards} 
+            sx={{ backgroundColor:"#252525", color: "white", elevation: 0 }}
+          >
+            <p>Name: {song.name}</p>
+            <p>Song Title: {song.songTitle}</p>
+            <p>Link: {song.link}</p>
+            <p>Comment: {song.comment}</p>
+          </Paper>
+        ))}
+      </Carousel>
+    {/* </section> */}
+    </>
   );
 }
